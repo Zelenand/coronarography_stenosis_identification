2293cb90d9b69cebe755c640a564eea035b55f09
Nikita Samarin <yormungandsam@yandex.ru>
2023-11-09 14:14:57 +0700

    yolo 10 epochs trained

--- controller.py
+++ controller.py
@@ -1,6 +1,12 @@
+import os
+
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QFileDialog, QMainWindow
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QFileDialog, QMainWindow, QLabel
+from PyQt5.QtGui import QPixmap, QImage
+from ultralytics import YOLO
+from PIL import Image, ImageQt
+import cv2
+
 import gui
 
 
@@ -12,9 +18,27 @@ class MainWindow(QMainWindow, gui.Ui_MainWindow):
         self.setFixedSize(self.size())
 
         self.pushButton_2.clicked.connect(self.choose_image)
+        self.pushButton.clicked.connect(self.detect_and_show)
+
+        self.model = YOLO("weights.pt")
+        self.img = None
+        self.HOME = os.getcwd()
 
     def choose_image(self):
-        pixmap = QPixmap(QFileDialog.getOpenFileName(self, 'Open File', 'C:/', 'PNG File (*.png);;JPG File *.jpg)')[0])
+        try:
+            self.img = Image.open(QFileDialog.getOpenFileName(self, 'Open File', self.HOME, 'Images (*.png *.jpg *.bmp)')[0])
+        finally:
+            pass
+
+    def detect_and_show(self):
+        if self.img:
+            results = self.model.predict(source=self.img)
+            Image.fromarray(results[0].plot()).save('image.png')
+            image = QPixmap('image.png')
+            self.im.setPixmap(image)
+            self.scrollArea.setWidget(self.im)
+
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
